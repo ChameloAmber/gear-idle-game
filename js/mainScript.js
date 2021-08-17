840fcf72f81ec17b3b895643136c2c4c3736417a
ChameloAmber <chameloamber@gmail.com>
2021-08-17 17:32:10 +0700

    It will work eventually

--- js/mainScript.js
+++ js/mainScript.js
@@ -8,7 +8,7 @@ var globalConstant = {
 };
 
 var metaData = {
-    build: "4.3"
+    build: "4.4"
 }
 
 var gameTicker = setInterval(gearGeneration, 1000 / globalConstant.gameTicksPerSecond)
--- js/visualController.js
+++ js/visualController.js
@@ -1,3 +1,5 @@
+import "../libraries/bignumber"
+
 function updateVisual()
 {
 	switch(globalDisplays.currentScreen)
