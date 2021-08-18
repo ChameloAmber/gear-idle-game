de4f5277cfba3a5d3f7915b87892f786758a2cd1
ChameloAmber <chameloamber@gmail.com>
2021-08-18 13:30:14 +0700

    css change #10

--- js/visualController.mjs
+++ js/visualController.mjs
@@ -6,6 +6,10 @@ export function updateVisual()
 	switch(globalDisplays.currentScreen)
 	{
 		case 0: 
+		document.getElementById("screenTower").className = "screenSwitcher-selected"
+		document.getElementById("screenLibrary").className = "screenSwitcher"
+		document.getElementById("screenLab").className = "screenSwitcher"
+		document.getElementById("screenGarden").className = "screenSwitcher"
 		for(let i = 0; i < 5; i++)
 		{
 			if(gears.state[i] == 1)
@@ -15,10 +19,17 @@ export function updateVisual()
                 document.getElementById("gear" + (i+1) + "Ratio").innerHTML = gears.ratio[i].toFixed(0) + ":1"
                 document.getElementById("gear" + (i+1) + "Progress").style.width = gears.progress[i].multipliedBy(100).toFixed(4) + "%"
 				document.getElementById("gear" + (i+1) + "Box").style.display = "block"
+				if(i == 0) {document.getElementById("screenTower").style.display = "block"}
+				if(i == 1) {document.getElementById("screenLibrary").style.display = "block"}
+				if(i == 2) {document.getElementById("screenLab").style.display = "block"}
+				if(i == 4) {document.getElementById("screenGarden").style.display = "block"}
 			}
 			else
 			{
 				document.getElementById("gear" + (i+1) + "Box").style.display = "none"
+				if(i == 1) {document.getElementById("screenLibrary").style.display = "none"}
+				if(i == 2) {document.getElementById("screenLab").style.display = "none"}
+				if(i == 4) {document.getElementById("screenGarden").style.display = "none"}
 			}
 		}
 		break;
