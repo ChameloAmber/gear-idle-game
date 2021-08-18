7b9fe26496928168e4b3bb4ebabcaf8b8dbebad2
ChameloAmber <chameloamber@gmail.com>
2021-08-18 13:11:16 +0700

    css change #8

--- js/mainScript.mjs
+++ js/mainScript.mjs
@@ -11,7 +11,7 @@ export var globalConstant = {
 };
 
 export var metaData = {
-    build: "7.7"
+    build: "7.8"
 }
 
 export var gameTicker = setInterval(gearGeneration, 1000 / globalConstant.gameTicksPerSecond)
