6076f31e58f1eded7f1ceff4dd36f1e59fad0441
ChameloAmber <chameloamber@gmail.com>
2021-08-18 13:51:12 +0700

    test onClick

--- js/mainScript.mjs
+++ js/mainScript.mjs
@@ -11,7 +11,7 @@ export var globalConstant = {
 };
 
 export var metaData = {
-    build: "7.11"
+    build: "7.12"
 }
 
 export var gameTicker = setInterval(gearGeneration, 1000 / globalConstant.gameTicksPerSecond)
