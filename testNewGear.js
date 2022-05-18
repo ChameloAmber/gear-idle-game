d60e881afd024abcd5d8be6ef2fb2c8dc46b12b7
ChameloAmber <chameloamber@gmail.com>
2022-05-19 02:56:39 +0700

    Test New Method

---
+++ testNewGear.js
@@ -0,0 +1,19 @@
+var gearsCurrent = [0, 0, 0, 0, 0];
+var gearsRatio = [BigNumber(10), BigNumber(2), BigNumber(3), BigNumber(4), BigNumber(5)];
+var gearsEnergy = [0, 0, 0, 0, 0];
+
+function Update() {
+
+    total = BigNumber(1)
+    for(i = 0; i < 5; i++) {
+        gearsCurrent[i] += 1 / 20
+        total = total.multipliedBy(gearsRatio[i])
+        if (total.isLessThanOrEqualTo(gearsCurrent[i])) {
+            gearsEnergy[i]++
+            gearsCurrent[i] = 0
+        }
+    }
+
+    console.log(gearsCurrent)
+    console.log(gearsEnergy)
+}
